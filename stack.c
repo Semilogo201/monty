55835242fd4de05d2e1051511e0152f53fa17036
owolabi oluwasemilogo <owolabioluwasemilogo04@gmail.com>
2024-01-17 09:44:46 -0800

    LIFO

---
+++ stack.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * r_stack - prints the top
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+*/
+void r_stack(stack_t **head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
